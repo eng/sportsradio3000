1fd23ca2a2367bcef8f1f2e003f0250925dbd2e8
Brian Eng <brian@brianeng.com>
2014-03-08 14:42:15 -0600

    back to pico2wave

--- app.rb
+++ app.rb
@@ -6,6 +6,9 @@ require 'espn'
 client = ESPN::Client.new(api_key: 'xcrr9vemtwr8u75msmg2sq8n')
 headlines = client.headlines(method: 'top')
 headlines.each do |headline|
-  `mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=en&q=$#{URI.escape(headline.description)}"`
-  # `pico2wave -w /home/pi/description.wav "#{headline.description}" && aplay /home/pi/description.wav`
+  # Google Translate sounds much better. But limited to 100 characters.
+  # `mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=en&q=$#{URI.escape(headline.description)}"`
+
+  # pico2wave doesn't sound as good. But can do whatever.
+  `pico2wave -w /home/pi/description.wav "#{headline.description}" && aplay /home/pi/description.wav`
 end
