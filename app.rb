453d0c0c6f4986fd472f38556af9995a839e594e
Brian Eng <brian@brianeng.com>
2014-03-08 14:37:52 -0600

    try using google translate

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'open-uri'
 require 'espn'
 
 client = ESPN::Client.new(api_key: 'xcrr9vemtwr8u75msmg2sq8n')
 headlines = client.headlines(method: 'top')
 headlines.each do |headline|
-  `pico2wave -w /home/pi/description.wav "#{headline.description}" && aplay /home/pi/description.wav`
+  `mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=en&q=$#{URI.escape(headline.description)}"`
+  # `pico2wave -w /home/pi/description.wav "#{headline.description}" && aplay /home/pi/description.wav`
 end
